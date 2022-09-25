9a955fcc73a0472d75a1cc1614d4ed566b5eeb25
Lasha Iakobadze <liakobadze@europebet.com>
2022-09-25 20:47:45 +0400

    remove unused code

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -6,9 +6,6 @@ import { ReactiveFormsModule } from '@angular/forms';
 import { DropdownDirective } from './directives/dropdown.directive';
 import { PlusComponent } from './components/plus/plus.component';
 import { ImageUploadComponent } from './components/image-upload/image-upload.component';
-import { ShortenPipe } from './pipes/shorten.pipe';
-import { LoaderComponent } from './components/loader/loader.component';
-
 
 
 @NgModule({
@@ -16,14 +13,12 @@ import { LoaderComponent } from './components/loader/loader.component';
     DropdownDirective,
     PlusComponent,
     ImageUploadComponent,
-    // LoaderComponent,
   ],
   exports: [
     CommonModule,
     ReactiveFormsModule,
     PlusComponent,
     ImageUploadComponent,
-    // LoaderComponent,
     DropdownDirective
   ]
 })
